cd1aca1b84b49fa7e1fb3179743140964dc940f8
Rio Foster <friotious@gmail.com>
2022-06-08 17:59:35 -0700

    Setup App.js with Router and Link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import { Route, Switch, Link, BrowserRouter } from 'react-router-dom'
+import BookRide from './BookRide/BookRide'
+import AllRides from './AllRides/AllRides'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      city: {
+        name: 'Rapid City',
+        key: null,
+      },
+      weather: {},
+      userRides: []
+    }
+  }
+
+  render() {
+    return (
+      <main>
+        <BrowserRouter>
+        <Nav />
+        <Switch>
+
+        <Route
+          exact path='/book-ride'
+          render={() => <BookRide />}
+        />
+        
+        <Route
+          exact path='/all-rides'
+          render={() =>   <AllRides />}
+        />
+
+        <Route
+          exact path='/'
+          render={() => {
+            return (
+              <div className ='home-page'>
+                <div></div>
+                <Link to='/book-ride' className='homepage-link' id='book-ride'>Book Ride</Link>
+                <div></div>
+                <Link to='/all-rides' className='homepage-link' id='all-rides'>All Rides</Link>
+              </div>
+            )
+          }}
+        />
+
+
+        </Switch>
+        </BrowserRouter>
+      </main>
+    )
+  }
 }
+
 
 export default App;
