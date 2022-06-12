43eafa5f06c7ceed331c262c6ff70db28c1edc11
Rio Foster <friotious@gmail.com>
2022-06-12 11:34:35 -0700

    Reformat

--- src/Components/AllRides/AllRides.js
+++ src/Components/AllRides/AllRides.js
@@ -1,40 +1,31 @@
-import React from 'react'
-import './AllRides.css'
-import Ride from '../Ride/Ride'
-
+import React from "react";
+import "./AllRides.css";
+import Ride from "../Ride/Ride";
 
 function AllRides({ allRides }) {
+  const displayAllRides = allRides.map((ride) => {
+    return <Ride key={ride.date} ride={ride} />;
+  });
 
-    const displayAllRides = allRides.map(ride => {
-        return (
-            <Ride key={ride.date} ride={ride} />
-        )
-    })
+  const displayPoints = () => {
+    const points = allRides.reduce((acc, ride) => {
+      acc += ride.points;
+      return acc;
+    }, 0);
+    return points;
+  };
 
-    const displayPoints = () => {
-        const points = allRides.reduce((acc, ride) => {
-            acc += ride.points
-            return acc
-        }, 0)
-        return points
-    }
-    
-    return (
-        <div className='AllRides'>
-             {!allRides[0] && <h2> You need to ride more...</h2> }
-            <div className='top-all-rides'>
-                {displayAllRides}
-            </div>
-            <div className='bottom-all-rides'>
-                <div className="total-points">
-                    <div>Total Points to date: {`${displayPoints()}`}</div>
-                </div>  
-            </div>
+  return (
+    <div className="AllRides">
+      {!allRides[0] && <h2> You need to ride more...</h2>}
+      <div className="top-all-rides">{displayAllRides}</div>
+      <div className="bottom-all-rides">
+        <div className="total-points">
+          <div>Total Points to date: {`${displayPoints()}`}</div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default AllRides
-
-
-
+export default AllRides;
