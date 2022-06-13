f5afbe7cc535f3c55621e9312691431e66a26fad
Rio Foster <friotious@gmail.com>
2022-06-13 09:47:37 -0700

    Refactor Components.js files

--- src/Components/BookRide/BookRide.js
+++ src/Components/BookRide/BookRide.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./BookRide.css";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 class BookRide extends Component {
   constructor(props) {
@@ -10,99 +10,93 @@ class BookRide extends Component {
         date: new Date().toLocaleDateString(),
         miles: "",
         diffMult: 1,
-        points: '',
+        points: "",
         rideWeather: this.props.weather,
       },
     };
   }
 
   getDiffMult = () => {
-      let num = 1;
-    const { temp, windChill, hasPrecip } = this.props.weather;
-    if (this.state.ride.diffMult === 0) {
-        if (hasPrecip) {num += 0.2;}
-        if (temp < 45 && temp > 32) {num += 0.1;}
-        if (temp <= 32) {num += 0.3;}
-        if (temp < 85) {num += 0.1;}
-        if (windChill < 45) {num += 0.1;}
-        if (temp > 45 && temp < 84 && !hasPrecip) {num -= 0.2;}
-        console.log(num.toFixed(1), 'num')
-        this.setState({ ride: {
-            ...this.state.ride,
-            diffMult: num.toFixed(1)
-        }})
-    } else {
-        return 
-    }
+    let num = 1;
+  const { temp, windChill, hasPrecip } = this.props.weather;
+  if (this.state.ride.diffMult === 0) {
+      if (hasPrecip) {num += 0.2;}
+      if (temp < 45 && temp > 32) {num += 0.1;}
+      if (temp <= 32) {num += 0.3;}
+      if (temp < 85) {num += 0.1;}
+      if (windChill < 45) {num += 0.1;}
+      if (temp > 45 && temp < 84 && !hasPrecip) {num -= 0.2;}
+      console.log(num.toFixed(1), 'num')
+      this.setState({ ride: {
+          ...this.state.ride,
+          diffMult: num.toFixed(1)
+      }})
+  } else {
+      return 
   }
-
+}
 
   componentDidMount = () => {
-   this.getDiffMult()
-   console.log(this.state.ride.points, 'points')
-}
+    this.getDiffMult();
+  };
 
-componentDidUpdate = () => {
-   console.log(this.state.ride.points, 'points')
-
-}
-
-
-saveRide = e => {
+  saveRide = (e) => {
     e.preventDefault();
-    const { addRide } = this.props
-    const newRide = { 
-        id: Date.now(),
-        ...this.state.ride 
-    }
+    const { addRide } = this.props;
+    const newRide = {
+      id: Date.now(),
+      ...this.state.ride,
+    };
     addRide(newRide);
     this.clearInputs();
-};
-    
-clearInputs = () => {
+  };
+
+  clearInputs = () => {
     this.setState({
-        ride: {
-            ...this.state.ride,
-            date: "",
-            miles: "",
-        },
+      ride: {
+        ...this.state.ride,
+        date: "",
+        miles: "",
+      },
     });
-};
+  };
 
   handleMilesChange = (e) => {
     this.setState({
       ride: {
         ...this.state.ride,
         miles: e.target.value,
-        points: e.target.value * this.state.ride.diffMult
-      }
+        points: e.target.value * this.state.ride.diffMult,
+      },
     });
   };
 
   handleDateChange = (e) => {
     this.setState({
-        ride: {
-            ...this.state.ride,
-            date: e.target.value
-        }
-    })
-  }
+      ride: {
+        ...this.state.ride,
+        date: e.target.value,
+      },
+    });
+  };
 
   render() {
-    const { ride: { date, miles, diffMult, points, rideWeather } } = this.state
+    const {
+      ride: { date, miles, diffMult, points, rideWeather },
+    } = this.state;
     return (
       <div className="BookRide">
         <div className="top-book-ride">
-          <div className="form-box" test-cy='form-box'>
+          <div className="form-box" test-cy="form-box">
             <h3>Book your Ride</h3>
             <p>Todays Temp: {rideWeather.temp} F</p>
             <p>Todays Multiple: {diffMult}</p>
-            <p test-cy='points'>points you will make: {points}</p>
+            <p test-cy="points">points you will make: {points}</p>
 
             <form className="form">
               <input
                 test-cy="input-miles"
-                id='test'
+                id="test"
                 type="text"
                 placeholder="Miles you will ride"
                 name="miles"
@@ -111,38 +105,35 @@ clearInputs = () => {
               />
               <input
                 type="text"
-                placeholder={new Date().toLocaleDateString()}qc
+                placeholder={new Date().toLocaleDateString()}
+                qc
                 name="date"
                 value={date}
                 onChange={(e) => this.handleDateChange(e)}
               />
-
             </form>
-            <button 
-                className='save-ride-button'
-                onClick={event => this.saveRide(event)}
-                test-cy='save-ride-button'>save ride</button>
+            <button
+              className="save-ride-button"
+              onClick={(event) => this.saveRide(event)}
+              test-cy="save-ride-button"
+            >
+              save ride
+            </button>
           </div>
         </div>
         <div className="bottom-book-ride"></div>
       </div>
     );
   }
-
-  //Calculate pointsFromRide method 'onCHange' of form
-
-  // Book ride button 'onCLick' should run add ride method from App
 }
 
 export default BookRide;
 
 BookRide.propTypes = {
-    weatherText: PropTypes.string.isRequired,
-    weatherIcon: PropTypes.node.isRequired,
-    hasPrecip: PropTypes.bool.isRequired,
-    temp: PropTypes.number.isRequired,
-    phrase: PropTypes.string.isRequired,
-    windChill: PropTypes.number.isRequired
-}
-
- 
+  weatherText: PropTypes.string.isRequired,
+  weatherIcon: PropTypes.node.isRequired,
+  hasPrecip: PropTypes.bool.isRequired,
+  temp: PropTypes.number.isRequired,
+  phrase: PropTypes.string.isRequired,
+  windChill: PropTypes.number.isRequired,
+};
