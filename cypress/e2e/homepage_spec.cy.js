c840deabb27e1ae9123f8f7041cc4c0028659bb1
Rio Foster <friotious@gmail.com>
2022-06-12 23:28:24 -0700

    Add basic homepage tests

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -1,12 +1,21 @@
-import weather from '../../src/'
 
 describe('home page', () => {
-
-  beforeEach(() => {
-        cy.intercept('GET', 'http://dataservice.accuweather.com/currentconditions/v1/351409?apikey=1sASGwIeUkRo5cXX5Lr4uJLFtMWdTGZB&details=true', weather).as("testData")
- )
+  it('successfully loads', () => {
+    cy.visit('http://localhost:3000/')
   })
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() => {
+        cy.intercept('GET', 'http://dataservice.accuweather.com/currentconditions/v1/351409?apikey=1sASGwIeUkRo5cXX5Lr4uJLFtMWdTGZB&details=true', 
+        { fixtures: 'weather.json' })
+        cy.visit('http://localhost:3000/')
+  })
+
+  it('should have a homepage', () => {
+    cy.url('http://localhost:3000/')
+  })
+
+  it('should have a navbar with the name "bicyclyr" links to book ride and all rides', () => {
+    cy.contains('Bicyclyr')
+    cy.get('.book-ride-text')
+      .should
   })
 })
